f107c54bdd28c44677be97fbffbd74272c3c9562
edmund2104 <112379234+edmund2104@users.noreply.github.com>
2022-09-18 22:55:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -30,14 +30,14 @@ def send_content_message(msg):
 def send_multi_message(msg):
     bot.reply_to(
         msg,
-        "<b>Non Muslim</b> \nSmall Pau \nChicken Puff \nCookies & Cream Cream Roll \n\n<b>Vegetarian Chinese</b> \nFried Vegetarian Bee Hoon \nVegetarian Spring Roll")
+        "<b>Non Muslim</b> \nChicken Curry Puff \nYam Pau \nDanish Apple \n\n<b>Vegetarian Chinese</b> \nFried Vegetarian Bee Hoon \nVegetarian Spring Roll")
 
 
 @bot.message_handler(commands=["mondaylunch", "Mondaylunch", "MondayLunch"])
 def send_multi_message(msg):
     bot.reply_to(
         msg,
-        "<b>Non Muslim</b> \n<u>Set A (Outration Set)</u> \n<u>Nasi Lemak Set</u> \nNasi Lemak Rice (80% white rice+20% brown rice) \nBaked Chicken Wing \nFried Fish Cake \nSambal Telur Goreng \nSambal Chilli Sauce Retort \nStir Fried Chye Sim with Bean Sprout \nRed Bean Sago \n\n<b>Vegetarian Chinese</b> \nSteamed Rice (80% white rice+20% brown rice) \nSambal Mock Chicken \nHot and Spicy Tau Hoo \nSauteed French Bean with Chinese Mushroom")
+        "<b>Non Muslim</b> \n<u>Set A (Outration Set)</u> \n<u>Ayam Goreng Berempah Set \nNasi Kerabu \nAyam Goreng Berempah \nIkan Bilis with Large Onion Sambal \nSavor Lodeh \nSop Ayam Bawang \n\nAyam Mee Soto Set \nYellow Noodle \nChicken Wing \nPotato Cake \nHardboiled Egg \nBeansprout \nSweet Potato and Ginger Dessert \n\n<b>Vegetarian Chinese</b> \nSteamed Rice (80% white rice+20% brown rice) \nSambal Mock Chicken \nHot and Spicy Tau Hoo \nSauteed French Bean with Chinese Mushroom")
 
 
 @bot.message_handler(
@@ -45,7 +45,7 @@ def send_multi_message(msg):
 def send_multi_message(msg):
     bot.reply_to(
         msg,
-        "<b>Non Muslim</b> \nMee Goreng Yellow Noodles \nBaked Chicken \nSambal Fried Fish Cake \n\n<b>Vegetarian Chinese</b> \nSteamed Rice (80% white rice+20% brown rice) \nStewed Mock Chicken with Chestnut \nStir Fried Mock Fungus Roll with Pineapple \nStir Fried Lady Finger")
+        "<b>Non Muslim</b> \nFried Rice with Chicken Set \nFried Rice \nBaked Chicken Chop \nWasabi Mayo \nSeaweed Chicken \nTasty Tientsin Cabbage \n\n<b>Vegetarian Chinese</b> \nSteamed Rice (80% white rice+20% brown rice) \nStewed Mock Chicken with Chestnut \nStir Fried Mock Fungus Roll with Pineapple \nStir Fried Lady Finger")
 
 
 @bot.message_handler(commands=[
@@ -54,14 +54,14 @@ def send_multi_message(msg):
 ])
 def send_multi_message(msg):
     bot.reply_to(
-        msg, "<b>Non Muslim</b> \nMee Tai Mak Goreng \nMee Tai Mak \nPrawn meat \negg \nbean sprout \nButter Cream Roll")
+        msg, "<b>Non Muslim</b> \nFried Mee \nYellow Noodles \nChicken Slice \nEgg \nChye Sim \nBean sprout \nCinnamon Danish
 
 
 @bot.message_handler(commands=["tuesdaylunch", "Tuesdaylunch", "TuesdayLunch"])
 def send_multi_message(msg):
     bot.reply_to(
         msg,
-        "<b>Non Muslim</b> \n<u>Set A (Outration Set)</u> \nPandan Rice with Chicken Manchrian Set \nPandan Rice \nChicken Opor \nStir fried pork with Thai Basil \nCabbage with Minced Chicken Lemak \nOld cucumber Soup \n<b>Set B</b> \nGinger Flower Braised Chicken Noodle Set \nIpoh Hor Fun \nGinger Flower Soya Base Gravy \nBaked Garlic Chicken \nPork Slice \nSou Peh Chye with Abalone Mushroom and Wolfberry \nLemon Barley Dessert \n<b>Vegetarian Chinese</b> \nVegetarian Mee Goreng \nMock Fish Curry \nFried Samosa Curry \nKang Kong with Chilli")
+        "<b>Non Muslim</b> \n<u>Set A (Outration Set)</u> \nPaprika Baked Chicken with Fusion Black Vinegar Paste Set \nFusion Black Vinegar Pasta \nPaprika Baked Chicken \nChicken Sausage \nButtered Colourful Vegetable \nCreme of Corn  \n\n<b>Set B</b> \nPorridge Set \nPlain Porridge \nSteamed Chicken with Salted Fish \nLor Bak \nChye Poh Omelette \nBraised Salted Vegetables \nPumpkin and Red Bean Dessert \n<b>Vegetarian Chinese</b> \nVegetarian Mee Goreng \nMock Fish Curry \nFried Samosa Curry \nKang Kong with Chilli")
 
 
 @bot.message_handler(
@@ -69,7 +69,7 @@ def send_multi_message(msg):
 def send_multi_message(msg):
     bot.reply_to(
         msg,
-        "<b>Non Muslim</b> \nSFIM Pinya Baked Chicken Set \nChili Tomato Fried Noodle \nChili Seafood Sauce \nPinya Baked Chicken \nFried Egg with Tomato \nBroccoli with Shrimp \n\n<b>Vegetarian Chinese</b> \nSteamed Rice (80% white rice+20% brown rice) \nMock Duck With Yam \nFried Mock Chicken with Tau See \nBroccoli with Corn Kernel")
+        "<b>Non Muslim</b> \nNasi Minyak with Ayam Opor Set \nNasi Minyak \nChicken Opor \nFish Masak Merah \nLong Bean Sambal  \n\n<b>Vegetarian Chinese</b> \nSteamed Rice (80% white rice+20% brown rice) \nMock Duck With Yam \nFried Mock Chicken with Tau See \nBroccoli with Corn Kernel")
 
 
 @bot.message_handler(commands=[
@@ -79,7 +79,7 @@ def send_multi_message(msg):
 def send_multi_message(msg):
     bot.reply_to(
         msg,
-        "<b>Non Muslim</b> \nBig Pau /nSausage Roll /nDanish Apple \n\n<b>Vegetarian Chinese</b> \nFried Kway Teow \nCookies & Cream Cream Roll")
+        "<b>Non Muslim</b> \nTau Sar Delight \nTeriyaki Chicken Pau \nMilk Chocolate Muffin  \n\n<b>Vegetarian Chinese</b> \nFried Kway Teow \nCookies & Cream Cream Roll")
 
 
 @bot.message_handler(
@@ -87,7 +87,7 @@ def send_multi_message(msg):
 def send_multi_message(msg):
     bot.reply_to(
         msg,
-        "<b>Non Muslim</b> \n<u>Set A </u> \nSausage and Corn Rice set \nSausage and Corn Rice \nChicken Masak Merah \nCrab Dumpling \nHomemade Otar \nCauliflower with green peas \nPotato and Carrot Soup \n\n<u>Set B</u> \nMee Siam Set \nBee Hoon \nMee Siam Gravy \nEgg \nChicken Wing \nFishcake \nBean sprouts \nTau Kwa \nTau Pok \nSnow Fungus with Longan Dessert \n\n<b>Vegetarian Chinese</b> \nSteamed Rice (80% white rice+20% brown rice) \Mock Char Siew \nStir Fried Vegetarian Golden Slice with Mushroom \nSou Peh Chye with Oyster Sauce")
+        "<b>Non Muslim</b> \n<u>Set A </u> \nBaked Black Pepper Chicken with Fried Noodle Set \nFried Noodle \nBake Black Pepper Chicken \nWantan \nStir Fried Cauliflower with Green Pepper \nChicken Soup with Hairy Gourd and Fungus  \n\n<u>Set B</u> \nBak Kut Teh Set \nSteamed Rice \nLor Bak or Lor Chicken \nLor Tau Pok \nBraised Salted Vegetables \nBak Kut Teh Soup \nSago Gula Melaka with Sweet Potatoes \n\n<b>Vegetarian Chinese</b> \nSteamed Rice (80% white rice+20% brown rice) \Mock Char Siew \nStir Fried Vegetarian Golden Slice with Mushroom \nSou Peh Chye with Oyster Sauce")
 
 
 @bot.message_handler(commands=[
@@ -96,7 +96,7 @@ def send_multi_message(msg):
 def send_multi_message(msg):
     bot.reply_to(
         msg,
-        "<b>Non Muslim</b> \nLemongrass TomYam Chicken with Pineapple Rice Set \nPineapple Rice \nLemongrass TomYam Chicken \nStir Fried Pork with Black Pepper Sauce \nBean Sprout with Tau Pok \n\n<b>Vegetarian Chinese</b> \nSteamed Rice (80% white rice+20% brown rice) \nBraised Mock Duck with Black Vinegar \nBraised Tau Kwa with Mock Char Siew \nBraised Hairy Gourd Lemak")
+        "<b>Non Muslim</b> \nKung Poh Chicken Set \nPineapple Rice \nBaked Chicken with Kung Poh Sauce \nStir Fried Pork with Spring Onion \nChye Sim with Oyster Sauce\n\n<b>Vegetarian Chinese</b> \nSteamed Rice (80% white rice+20% brown rice) \nBraised Mock Duck with Black Vinegar \nBraised Tau Kwa with Mock Char Siew \nBraised Hairy Gourd Lemak")
 
 
 @bot.message_handler(commands=[
@@ -106,7 +106,7 @@ def send_multi_message(msg):
 def send_multi_message(msg):
     bot.reply_to(
         msg,
-        "<b>Non Muslim</b> \nFried Hong Kong Noodles \nInstant Noodles \nEgg \nPrawn Meat \nChicken Ham \nCarrot /nBean Sprout /nOnion \nCrab Ball \nChicken Meatball \n\n<b>Vegetarian Chinese</b> \nHamburger Bun \nMargarine \nPandan Pao \nRed Bean Pao \nVegetarian Ham")
+        "<b>Non Muslim</b> \nJavanese Fried Noodle \nEgg Noodle \nChicken slice \nEgg \nCarrot \nCabbage \nBean sprout \n Crab Dumplings \n\n<b>Vegetarian Chinese</b> \nHamburger Bun \nMargarine \nPandan Pao \nRed Bean Pao \nVegetarian Ham")
 
 
 @bot.message_handler(
@@ -114,7 +114,7 @@ def send_multi_message(msg):
 def send_multi_message(msg):
     bot.reply_to(
         msg,
-        "<b>Non Muslim</b> \n<u>Set A</u> \nGoing Green Special Lunch - Lu Dou Fan Set \nRice \nLu Dou \nBraised Tau Kwa \nButtered Broccoli \nButtered Carrot \nButtered Corn \nRed Dates Chicken Broth \n\n<u>Set B</u> \nLaksa Set \nLaksa Bee Hoon \nTau Pok \nFried Chicken Wing \nBean Sprout \nHard Boiled Egg \nFish Cake \nSago Gula Melaka with Sweet Potatoes \n\n<b>Vegetarian Chinese</b> \nVegetarian Fried Rice (80% White and 20% Brown Rice) \nVegetarian Crispy Chicken \nVegetarian Chilli Fish Balls \nStir Fried Broccoli with Chinese Mushroom")
+        "<b>Non Muslim</b> \n<u>Set A</u> \nChicken Char Siew Rice Set \nChicken Rice \nBaked Char Siew Chicken \Lor Egg \nStir Fried Peh Chye with Carrot and Tang Hoon \nRadish Cuttlefish Soup  \n\n<u>Set B</u> \nFried Fish Bee Hoon Soup Set \nLaksa Bee Hoon \nBee Hoon Soup \nFried Fish Fillet \nSteamed Crab Meat \nSou Peh Chye \nSteamed Tau Hoo \nTomato Wedges \nRed Bean Sago \n\n<b>Vegetarian Chinese</b> \nVegetarian Fried Rice (80% White and 20% Brown Rice) \nVegetarian Crispy Chicken \nVegetarian Chilli Fish Balls \nStir Fried Broccoli with Chinese Mushroom")
 
 
 @bot.message_handler(commands=[
@@ -123,7 +123,7 @@ def send_multi_message(msg):
 def send_multi_message(msg):
     bot.reply_to(
         msg,
-        "<b>Non Muslim</b> \nLong Bean Fried Rice Set \nLong Bean Fried Rice \nAyam Assam Melaka \nOtar Fish Cake \nStir Fried Chye Sim with Tau Kee and Carrot \n\n<b>Vegetarian Chinese</b> \nSteamed Rice (80% white rice+20% brown rice) \nStewed Mock Chicken with Potato \nBraised Tau Hoo With Mushroom \nFried Bayam with Carrot")                                                                                                                                           
+        "<b>Non Muslim</b> \nRosemary Chicken with Roast Potatoes Set \nBaked Potato with Herbs \nBaked Rosemary Chicken \nFish NUgget \nButtered Mixed Vegetables \n\n<b>Vegetarian Chinese</b> \nSteamed Rice (80% white rice+20% brown rice) \nStewed Mock Chicken with Potato \nBraised Tau Hoo With Mushroom \nFried Bayam with Carrot")                                                                                                                                           
 @bot.message_handler(commands=[
     "fridaybreakfast", "FridayBreakfast", "Fridaybf", "fridaybf",
     "Fridaybreakfast"
@@ -131,14 +131,14 @@ def send_multi_message(msg):
 def send_multi_message(msg):
     bot.reply_to(
         msg,
-"<b>Non Muslim</b> \nChar Siew Pau \nTau Sar Delight \nPain Au Raisin \n\n<b>Vegetarian Chinese</b> \nFried Macaroni \nVegetarian Spring Rolls")
+"<b>Non Muslim</b> \nUCMZ \nHamburger Bun \nMagarine \nBreaded Chicken Pattie \nSausage Roll \nPain Au Raisin \n\n<b>Vegetarian Chinese</b> \nFried Macaroni \nVegetarian Spring Rolls")
 
 
 @bot.message_handler(commands=["fridaylunch", "Fridaylunch", "FridayLunch"])
 def send_multi_message(msg):
     bot.reply_to(
         msg,
-        "<b>Non Muslim</b> \n<u>Set A </u> \nWasabi Fish Burger with Buffalo Wing Set \nHamburger \nFrench Fries \nFried Breaded Chicken Burger \nBaked Buffalo Chicken Wing Stick \nButtered Corn \nCreme of Corn \n\n<b>Vegetarian Chinese</b> \nSteamed Rice (80% white rice+20% brown rice) \nSweet and Sour Mock Fish \nTau Kwa Sambal \nSou Peh Chye with Oyster Sauce")
+        "<b>Non Muslim</b> \n<u>Set A </u> \nHainanese Mutton Stew with Rice \nSteamed Rice \nHainanese Mutton Stew \nSteamed Tau Hoo with Broad Bean Sauce \nStir Fried Honey Peas with Mixed Vegetables \nCheng Teng  \n\n<b>Vegetarian Chinese</b> \nSteamed Rice (80% white rice+20% brown rice) \nSweet and Sour Mock Fish \nTau Kwa Sambal \nSou Peh Chye with Oyster Sauce")
 
 
 @bot.message_handler(
@@ -146,7 +146,7 @@ def send_multi_message(msg):
 def send_multi_message(msg):
     bot.reply_to(
         msg,
-        "<b>Non Muslim</b> \nChar Siew Chicken Rice Set \nChicken Rice \nBaked Char Siew Chicken \nFried Egg with Sausage \nNgoh Hiang \nStir Fried Honey Peas with Mixed Vegetables \n\n<b>Vegetarian Chinese</b>")
+        "<b>Non Muslim</b> \nBee Hoon Goreng Set \nBee Hoon Goreng \nBaked Chicken Wing \nFried Fish Cake \nTau Kwa Goreng Sambal Tomato \nSambal Sauce Retort \n\n<b>Vegetarian Chinese</b>")
 
 
 bot.polling()
